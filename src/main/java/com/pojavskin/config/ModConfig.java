d0da76e646ef6f3e724c44646968510dc003f37a
minesd622-cmd <minesd622@gmail.com>
2025-12-20 02:18:20 +0530

    AI Forge Sync: PojavSkinReforged

---
+++ src/main/java/com/pojavskin/config/ModConfig.java
@@ -0,0 +1,11 @@
+package com.pojavskin.config;
+
+public class ModConfig {
+    public boolean enableCustomSkins = true;
+    public boolean enableCapeSupport = true;
+    public boolean cacheSkins = true;
+    public String skinApiUrl = "https://minotar.net/skin/%s";
+    public String capeApiUrl = "https://capes.example.com/%s.png";
+    public int cacheExpirationDays = 7;
+    public boolean debugMode = false;
+}
